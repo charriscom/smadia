60081be2ca1aacd814a1c8ddf7d755e81d3b8d85
Juan Carlos Roca <jcroca@hotmail.com>
2011-03-28 18:58:28 -0500

    add devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smadia::Application.config.secret_token = '81208effc15319858e964a9a59c6f98ae3dda7466c750c7fd711e54211e0477ac99801564abe65bcb6416fc11122fd9cdfa88d3207b249ba3b91df3bec1a0711'
