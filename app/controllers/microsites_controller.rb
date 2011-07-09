f1c1de84e84d15325555ad7667b56a34a73de581
Juan Carlos Roca <jcroca@hotmail.com>
2011-07-09 12:15:39 -0500

    microsites fix

--- app/controllers/microsites_controller.rb
+++ app/controllers/microsites_controller.rb
@@ -2,15 +2,14 @@ class MicrositesController < ApplicationController
   before_filter :menu_bar
   def show
     if @microsite = Microsite.find_by_name(params[:name])
-      unless @microsite.external_page
-        return redirect_to root_path 
-      end
+      return redirect_to root_path unless @microsite.external_page
       @external = true
-      render :layout => false
+      @form = @microsite.form_details
+      render :layout => false if @external
     end
     if @microsite = Microsite.find_by_id(params[:id])
       @external = false
+      @form = @microsite.form_details
     end
-    @form = @microsite.form_details
   end
 end
