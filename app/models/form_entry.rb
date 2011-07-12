5c68d51e93a7eefe62391462bbc65214256d7b7c
Juan Carlos Roca <jcroca@hotmail.com>
2011-07-12 00:05:40 -0500

    form validations

--- app/models/form_entry.rb
+++ app/models/form_entry.rb
@@ -1,4 +1,6 @@
 class FormEntry < ActiveRecord::Base
-  validates_confirmation_of :email
-  validates_presence_of :name, :phone
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_presence_of :name, :last_name, :phone, :mobile, :identifier
+  
+    
 end
