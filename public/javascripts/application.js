89b76c971ce06ad38a58362c8fb50bb4e68738ab
Daniel Osorio <danielosorio83@gmail.com>
2012-03-10 13:41:39 -0500

    post to external CRM

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,3 +6,72 @@ jQuery.fn.toggleAttr = function(attr) {
   $this.attr(attr) ? $this.removeAttr(attr) : $this.attr(attr, attr);
  });
 };
+
+$(function(){
+  $(".contact_form").submit(function(evt){
+    var $form = $(this);
+    var hash = {
+      name: $form.find("input[name='form_entry[name]']").val(),
+      last_name: $form.find("input[name='form_entry[last_name]']").val(),
+      phone: $form.find("input[name='form_entry[phone]']").val(),
+      mobile: $form.find("input[name='form_entry[mobile]']").val(),
+      country: $form.find("input[name='form_entry[country]']").val(),
+      city: $form.find("input[name='form_entry[city]']").val(),
+      mail: $form.find("input[name='form_entry[email]']").val(),
+      treatment: getTreatmentId($form.find("select[name='form_entry[service]']").find("option:selected").val()),
+      sorce: 13
+    };
+    var url = "http://crm.smadiaclinic.com/up.php";
+    $.ajax({
+      type: 'GET',
+      url: url,
+      data: hash
+    });
+  });
+})
+
+getTreatmentId = function(service){
+  var id = TREATMENTS_ID[service];
+  return (id == undefined) ? "" : id;
+}
+
+TREATMENTS_ID = {
+  "Laserlipolisis": 3,
+  "Hidrolipoclasia": 4,
+  "Ultracavitación": 10,
+  "Carboxiterapia": 11,
+  "Drenaje Linfático Manual": 12,
+  "Presoterapia": 13,
+  "Radiofrecuencia Corporal": 15,
+  "Masaje Relajante": 18,
+  "Momificación": 19,
+  "Peeling": 20,
+  "Fotodepilación": 21,
+  "Depilación con Cera": 22,
+  "Escleroterapia": 25,
+  "Intradermoterapia": 26,
+  "Sueroterapia": 30,
+  "Microdermoabrasion": 6,
+  "Radiofrecuencia Facial": 7,
+  "Laser RFID": 14,
+  "Hidratación": 16,
+  "Higiene": 17,
+  "Fotodepilación": 23,
+  "Depilación con Cera": 24,
+  "Intradermoterapia": 27,
+  "Tatuaje de Cejas": 28,
+  "Plasma": 29,
+  "Microdermoinfusión": 41,
+  "Softlift": 42,
+  "Botox": 43,
+  "Abdominoplastia": 9,
+  "Abdominoplastia con Liposucción": 32,
+  "Liposucción": 33,
+  "Mamoplastia de aumento": 34,
+  "Mamoplastia de Reducción": 35,
+  "Mastopexia (levantamiento de mamas)": 36,
+  "Blefaroplastia": 37,
+  "Lifting": 38,
+  "Otoplastia": 39,
+  "Rinoplastia": 40
+}
